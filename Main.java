331e5daf653f70713e0ac93954cad8a6d4a36eed
annsolou <solou.testing@gmail.com>
2021-05-27 07:07:03 +0200

    Main.java added

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int stoimostBileta = 5000; // в рублях
+        int bonus = 20;
+        int result = stoimostBileta / bonus;
+        System.out.println(result);
+    }
+}
